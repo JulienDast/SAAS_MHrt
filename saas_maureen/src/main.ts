e9b3e32613e4204f9c81206ff3bb212496d05c2b
Dast <julien@mirror404.com>
2024-11-11 17:01:58 +0100

    Cors

--- saas_maureen/src/main.ts
+++ saas_maureen/src/main.ts
@@ -6,7 +6,12 @@ async function bootstrap() {
 
 
   app.enableCors({
-    origin: process.env.CORS_ORIGIN
+    origin: [
+      process.env.CORS_ORIGIN, 
+      process.env.CORS_ORIGIN_PROD, 
+    ],
+    credentials: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   });
 
   await app.listen(process.env.PORT ?? 5000);
